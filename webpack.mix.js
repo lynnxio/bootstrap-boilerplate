2e65603b05ef70a53b139eb5511743e9ba3af2c9
QuixDevf <zunnur.brad@gmail.com>
2022-03-23 16:29:50 +0800

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,48 @@
+const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/site.js', 'public/js')
+    .sass('resources/sass/styles.scss', 'public/css')
+    .purgeCss({
+        extend: {
+            content: [
+                './resources/**/*.antlers.html',
+                './resources/**/*.blade.php',
+                './resources/**/*.vue',
+                './content/**/*.md'
+            ]
+        },
+    })
+
+
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | Statamic Control Panel
+ |--------------------------------------------------------------------------
+ |
+ | Feel free to add your own JS or CSS to the Statamic Control Panel.
+ | https://statamic.dev/extending/control-panel#adding-css-and-js-assets
+ |
+ */
+
+// mix.js('resources/js/cp.js', 'public/vendor/app/js')
+//    .postCss('resources/css/cp.css', 'public/vendor/app/css', [
+//     require('postcss-import'),
+//     require('tailwindcss/nesting'),
+//     require('tailwindcss'),
+// ])
